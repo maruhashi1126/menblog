a17e0c50d723d44f5b76b9d06937c3fa9b36d004
maruhashi1126 <97351526+maruhashi1126@users.noreply.github.com>
2022-01-11 14:52:53 +0900

    Add files via upload

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST',  'localhost');//データベースのホスト名またはIPアドレス
+define('DB_USER',  'riyou');//MySQLのユーザー名
+define('DB_PASSWD',  'Maru11260841');//MySQLのパスワード
+define('DB_NAME',  'men,s');//データベース名
+?>
